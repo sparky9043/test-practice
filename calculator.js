1537d67697bd84734a23ea5c2c7e4695a92ef9bc
sparky9043 <sparky9043@gmail.com>
2024-09-24 16:04:48 +0900

    Add divide function to calculator

--- calculator.js
+++ calculator.js
@@ -6,6 +6,11 @@ const calculator = {
 
   subtract: function(a, b) {
     return a - b;
+  },
+
+  divide: function(a, b) {
+    if (b === 0) return undefined;
+    return a / b;
   }
 };
 
--- calculator.test.js
+++ calculator.test.js
@@ -6,4 +6,7 @@ test('Should return proper calculation', () => {
   expect(calculator.add(3, -4)).toBe(-1);
   expect(calculator.subtract(12, 5)).toBe(7);
   expect(calculator.subtract(3, 10)).toBe(-7);
+  expect(calculator.divide(12, 3)).toBeCloseTo(4);
+  expect(calculator.divide(1, 3)).toBeCloseTo(0.3333333);
+  expect(calculator.divide(1, 0)).toBeUndefined();
 });
