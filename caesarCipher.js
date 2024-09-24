4cc4ca7811054382784b76fb8e2bd937a199ce6d
sparky9043 <sparky9043@gmail.com>
2024-09-24 17:18:54 +0900

    Use forEach instead of for loop

--- caesarCipher.js
+++ caesarCipher.js
@@ -2,14 +2,31 @@ function caesarCipher(string, factor) {
 
   const getShiftedString = (string) => {
     let copy = '';
-    for (let i = 0; i < string.length; i++) {
-      const shiftedString = String.fromCharCode(
-        string.charCodeAt(i) + factor
-      );
-      copy += shiftedString;
+
+    const checkUpperOrLowerCase = (string, factor) => {
+      const characters = string.split('');
+
+      characters.forEach((char, index) => {
+        const characterCode = char.charCodeAt(0);
+        if (characterCode >= 65 && characterCode <= 90) {
+
+          const convertedChar = String.fromCharCode(characterCode + factor);
+          characters[index] = convertedChar;
+
+        } else if (characterCode >= 97 && characterCode <= 122) {
+
+          const convertedChar = String.fromCharCode(characterCode + factor);
+          characters[index] = convertedChar;
+
+        }
+        
+      });
+
+
+      return characters.join('');
     }
 
-    return copy;
+    return checkUpperOrLowerCase(string, factor);
   }
 
   const shiftedString = getShiftedString(string);
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -4,5 +4,4 @@ test('Should exist', () => {
 
   expect(caesarCipher('ABC', 3)).toBe("DEF");
   expect(caesarCipher('abc', 3)).toBe('def');
-  expect(caesarCipher('ABC', 5)).toBe('FGH');
 });
